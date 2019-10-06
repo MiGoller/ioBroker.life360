ccdb7b767c9abe6aefacb3281da2f53b3fdfdfce
Skunkworx <1272642+MiGoller@users.noreply.github.com>
2019-10-06 11:25:19 +0200

    Minor updates regarding logging.

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ class Life360 extends utils.Adapter {
         try {
             life360Connector.disablePolling();
             life360Connector.disconnect();
-
+            this.setState("info.connection", false);
             this.log.info("cleaned everything up...");
             callback();
         } catch (e) {
